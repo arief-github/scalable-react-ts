21adf62df4ffe8fdb4dbf66270bb0a40da5cf8b1
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2024-08-10 00:41:20 +0700

    add: lazy loader

--- src/views/animal/ViewAnimalPhotos.tsx
+++ src/views/animal/ViewAnimalPhotos.tsx
@@ -1,4 +1,5 @@
 import useFetchAnimals from "@/hooks/useFetchAnimals";
+import LazyLoader from "@/components/common/LazyLoader";
 
 function ViewAnimalPhotos() {
 	const {
@@ -19,12 +20,12 @@ function ViewAnimalPhotos() {
 		<div className='my-8 mx-auto max-w-2xl'>
 			<div className='flex justify-center gap-8'>
 				{isFetchDogStatusIdle || isFetchCatStatusIdle ? <p>Selamat Datang</p> : null}
-				{isFetchDogStatusPending || isFetchCatStatusPending ? <p>Memuat data....</p> : null}
+				{<LazyLoader show={isFetchDogStatusPending || isFetchCatStatusPending} delay={400} />}
 				{isFetchDogStatusError || isFetchCatStatusError ? <p>Ada Masalah saat memuat data</p> : null}
 				{isFetchDogStatusSuccess || isFetchCatStatusSuccess ? (
 					<div className='flex gap-8'>
-						<div className='w-100'>{cat ? <img className='h-64 w-full object-cover' src={cat} /> : null}</div>
-						<div className='w-100'>{dog ? <img className='h-64 w-full object-cover' src={dog} /> : null}</div>
+						<div className='w-100'>{cat ? <img className='h-64 w-full object-cover' src={cat} alt='cat image' /> : null}</div>
+						<div className='w-100'>{dog ? <img className='h-64 w-full object-cover' src={dog} alt='dog image' /> : null}</div>
 					</div>
 				) : null}
 			</div>
